332ffb75a16d5dc9cc45326a1384e9ef25cc2e07
Piero <pierojacinto@gmail.com>
2023-07-01 21:13:17 -0300

    agrego foto en servidor

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 
+from datetime import datetime
+
 app = Flask(__name__)
 mysql = MySQL()
 
@@ -40,6 +42,13 @@ def store():
     _correo = request.form['txtCorreo'] 
     _foto = request.files['txtFoto']
 
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    if _foto != "":
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
     sql = "insert INTO EMPLEADOS (nombre, correo, foto) values (%s,%s,%s);"
     datos = ( _nombre, _correo, _foto.filename )
 
@@ -49,6 +58,7 @@ def store():
     conn.commit()
 
     return redirect('/')
+
 
 
 if __name__ == '__main__':
